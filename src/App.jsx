cb60c43ee8e1d0165747522a987e5a58201a74d0
Dana <126463047+hey-dane@users.noreply.github.com>
2023-06-13 04:25:13 -0700

    added data and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
-// function App() {
-//   const [count, setCount] = useState(0);
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
-//   return <div></div>;
-// }
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      <div>
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
-// export default App;
-// delete?
+export default App;
